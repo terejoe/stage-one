1cece77308d79e6181f3151da3e2da19c1db7745
terejoe <nzeamalunkechinyere@gmail.com>
2022-10-27 13:25:56 +0100

    edited slack username in button feature

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -8,7 +8,7 @@ const Button = () => {
         <a href="https://twitter.com/tere__joe?s=09" id="twitter"className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Twitter Link</a>
         <a href="https://training.zuri.team/" id="btn__zuri" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Zuri Team</a>
         <a href="https://books.zuri.team" id="books" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<terejoe>" id="book__python" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Python Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=Terejoe" id="book__python" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Python Books</a>
         <a href="https://background.zuri.team" id="pitch" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Background Check for Coders</a>
         <a href="https://books.zuri.team/design-rules" id="book__design" className="bg-[#EAECF0] hover:bg-[#D0D5DD] px-8 py-6 text-sm md:text-lg font-medium text-[#101828] rounded-lg border-solid text-center">Design Books</a>
       </div>
